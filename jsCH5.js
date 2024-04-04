c9d8b7b2857d8fda83d3fb13c89acad6b96adfdb
DimasCW <cdimas292@gmail.com>
2024-04-04 20:57:58 +0700

    first commit

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEvenListener("click", function video(){
+    alert("ini video asomaso")
+});
+
+Audio.addEvenListener("click", function audio(){
+    alert("ini audio asomasioi")
+})
+
+gambar.addEvenListener("click", function gambar(){
+    alert("gambar asoaoddf")
+})
